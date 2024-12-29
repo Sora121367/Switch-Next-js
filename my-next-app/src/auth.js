596caad434a5d99e8decc86786b3a1cba608769f
Un Sovannara <unsovannara12@gmail.com>
2024-12-29 10:32:55 +0700

    Delete my-next-app directory

--- my-next-app/src/auth.js
+++
@@ -1,6 +0,0 @@
-import NextAuth from "next-auth"
-import Facebook from "next-auth/providers/facebook"
-import Google from "next-auth/providers/google"
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Facebook,Google],
-})
