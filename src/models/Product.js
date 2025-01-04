bb047fad1440e65c6033c3c1af648c009e0742c7
LuckyHeng9 <123loemheng@gmail.com>
2025-01-04 19:12:44 +0700

    new update

--- src/models/Product.js
+++ src/models/Product.js
@@ -6,14 +6,15 @@ const ProductSchema = new mongoose.Schema(
     price: { type: Number, required: true },
     description: { type: String, required: true },
     instock: { type: Boolean, required: true },
-    method_payment: { type: String, required: true },
-    category: { type: String, default: "Uncategorized" },
-    size: { type: String, required:true },
+    //method_payment: { type: String, required: true },
+    //category: { type: String, default: "Uncategorized" },
+    //size: { type: String, required:true },
     image: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-const Product = mongoose.models.Product || mongoose.model("Product", ProductSchema);
+const Product =
+  mongoose.models.Product || mongoose.model("Product", ProductSchema);
 
 export default Product;
